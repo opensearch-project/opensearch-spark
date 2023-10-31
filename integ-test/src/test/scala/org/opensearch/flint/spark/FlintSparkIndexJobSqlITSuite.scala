d36c7e85658a543af047905347a469b7047d0dcc
Chen Dai <daichen@amazon.com>
2023-10-31 14:05:42 -0700

    Add IT for MV
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- integ-test/src/test/scala/org/opensearch/flint/spark/FlintSparkIndexJobSqlITSuite.scala
+++ integ-test/src/test/scala/org/opensearch/flint/spark/FlintSparkIndexJobSqlITSuite.scala
@@ -42,7 +42,8 @@ class FlintSparkIndexJobSqlITSuite extends FlintSparkSuite with Matchers {
         }
         .assertIndexData(indexData => indexData should have size 5)
         .stopStreamingJob()
-        .run(s""" INSERT INTO $testTable VALUES
+        .run(s"""
+             | INSERT INTO $testTable VALUES
              | (TIMESTAMP '2023-10-01 05:00:00', 'F', 35, 'Vancouver')
              |""".stripMargin)
         .run(s"RECOVER INDEX JOB $testSkippingIndex")
@@ -65,10 +66,35 @@ class FlintSparkIndexJobSqlITSuite extends FlintSparkSuite with Matchers {
         }
         .assertIndexData(indexData => indexData should have size 5)
         .stopStreamingJob()
-        .run(s""" INSERT INTO $testTable VALUES
+        .run(s"""
+             | INSERT INTO $testTable VALUES
              | (TIMESTAMP '2023-10-01 05:00:00', 'F', 35, 'Vancouver')
              |""".stripMargin)
         .run(s"RECOVER INDEX JOB $testCoveringIndex")
+        .assertIndexData(indexData => indexData should have size 6)
+    }
+  }
+
+  test("recover materialized view refresh job") {
+    withFlintIndex(testMvIndex) { assertion =>
+      assertion
+        .run { checkpointDir =>
+          s""" CREATE MATERIALIZED VIEW $testMv
+             | AS
+             | $testMvQuery
+             | WITH (
+             |   auto_refresh = true,
+             |   checkpoint_location = '${checkpointDir.getAbsolutePath}'
+             | )
+             |""".stripMargin
+        }
+        .assertIndexData(indexData => indexData should have size 5)
+        .stopStreamingJob()
+        .run(s"""
+                | INSERT INTO $testTable VALUES
+                | (TIMESTAMP '2023-10-01 05:00:00', 'F', 35, 'Vancouver')
+                |""".stripMargin)
+        .run(s"RECOVER INDEX JOB $testMvIndex")
         .assertIndexData(indexData => indexData should have size 6)
     }
   }
@@ -87,6 +113,9 @@ class FlintSparkIndexJobSqlITSuite extends FlintSparkSuite with Matchers {
     }
   }
 
+  /**
+   * Recover test assertion helper that de-duplicates test code.
+   */
   private class AssertionHelper(flintIndexName: String, checkpointDir: File) {
 
     def run(createIndex: File => String): AssertionHelper = {
@@ -109,17 +138,12 @@ class FlintSparkIndexJobSqlITSuite extends FlintSparkSuite with Matchers {
       spark.streams.get(findJobId(flintIndexName)).stop()
       this
     }
-  }
 
-  private def startMaterializedViewIndexJob(checkpointDir: File): Unit = {
-    sql(s"""
-           | CREATE MATERIALIZED VIEW $testMv
-           | AS
-           | $testMvQuery
-           | WITH (
-           |   auto_refresh = true,
-           |   checkpoint_location = '${checkpointDir.getAbsolutePath}'
-           | )
-           |""".stripMargin)
+    private def findJobId(indexName: String): String = {
+      val job = spark.streams.active.find(_.name == indexName)
+      job
+        .map(_.id.toString)
+        .getOrElse(throw new RuntimeException(s"Streaming job not found for index $indexName"))
+    }
   }
 }
