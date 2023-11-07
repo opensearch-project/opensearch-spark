71ccd859d5a863caa777208262bad552c7125523
Peng Huo <penghuo@gmail.com>
2023-11-06 21:19:29 -0800

    address comments
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- flint-core/src/main/scala/org/opensearch/flint/core/storage/OpenSearchScrollReader.java
+++ flint-core/src/main/scala/org/opensearch/flint/core/storage/OpenSearchScrollReader.java
@@ -18,11 +18,15 @@ import org.opensearch.flint.core.FlintOptions;
 import org.opensearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * {@link OpenSearchReader} using scroll search. https://opensearch.org/docs/latest/api-reference/scroll/
  */
 public class OpenSearchScrollReader extends OpenSearchReader {
+
+  private static final Logger LOG = Logger.getLogger(OpenSearchScrollReader.class.getName());
 
   /** Default scroll context timeout in minutes. */
   public static final TimeValue DEFAULT_SCROLL_TIMEOUT = TimeValue.timeValueMinutes(5L);
@@ -63,6 +67,8 @@ public class OpenSearchScrollReader extends OpenSearchReader {
       }
     } catch (OpenSearchStatusException e) {
       // OpenSearch throw exception if scroll already closed. https://github.com/opensearch-project/OpenSearch/issues/11121
+      LOG.log(Level.WARNING, "close scroll exception, it is a known bug https://github" +
+          ".com/opensearch-project/OpenSearch/issues/11121.", e);
     }
   }
 }
