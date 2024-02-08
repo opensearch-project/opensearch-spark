d0f5754f6e491c92deba88d9042e26ac60eb978f
Kaituo Li <kaituo@amazon.com>
2024-02-08 13:50:32 -0800

    added Java doc
    Signed-off-by: Kaituo Li <kaituo@amazon.com>

--- spark-sql-application/src/test/scala/org/apache/spark/sql/util/MockEnvironment.scala
+++ spark-sql-application/src/test/scala/org/apache/spark/sql/util/MockEnvironment.scala
@@ -5,6 +5,18 @@
 
 package org.apache.spark.sql.util
 
+/**
+ * A mock implementation of `EnvironmentProvider` for use in tests, where environment variables can be predefined.
+ *
+ * @param inputMap A map representing the environment variables (name -> value).
+ */
 class MockEnvironment(inputMap: Map[String, String]) extends EnvironmentProvider {
+  /**
+   * Retrieves the value of an environment variable from the input map or returns a default value if not present.
+   *
+   * @param name    The name of the environment variable.
+   * @param default The default value to return if the environment variable is not set in the input map.
+   * @return The value of the environment variable from the input map if it exists, otherwise the default value.
+   */
   def getEnvVar(name: String, default: String): String = inputMap.getOrElse(name, default)
 }
