fe0c7c426fe2fd9711ce2a4f01f9f6c66c0fe0f2
Sean Kao <seankao@amazon.com>
2024-04-18 12:47:05 -0700

    Bump Flint version to 0.4.0 (#311)
    * Bump Flint version to 0.4.0
    
    Signed-off-by: Sean Kao <seankao@amazon.com>
    
    * dependency on opensearch-sql 2.13
    
    Signed-off-by: Sean Kao <seankao@amazon.com>
    
    ---------
    
    Signed-off-by: Sean Kao <seankao@amazon.com>

--- flint-core/src/main/scala/org/opensearch/flint/core/FlintVersion.scala
+++ flint-core/src/main/scala/org/opensearch/flint/core/FlintVersion.scala
@@ -17,6 +17,7 @@ object FlintVersion {
   val V_0_1_0: FlintVersion = FlintVersion("0.1.0")
   val V_0_2_0: FlintVersion = FlintVersion("0.2.0")
   val V_0_3_0: FlintVersion = FlintVersion("0.3.0")
+  val V_0_4_0: FlintVersion = FlintVersion("0.4.0")
 
-  def current(): FlintVersion = V_0_3_0
+  def current(): FlintVersion = V_0_4_0
 }
