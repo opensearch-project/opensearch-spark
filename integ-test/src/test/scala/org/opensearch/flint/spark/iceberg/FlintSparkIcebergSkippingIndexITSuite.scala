dd17129f9cee2e78baf091f7343d46ca92794e72
Peng Huo <penghuo@gmail.com>
2024-05-14 16:24:38 -0700

    Ignore FlintSparkWindowingFunctionITSuite and IcebergIT
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- integ-test/src/test/scala/org/opensearch/flint/spark/iceberg/FlintSparkIcebergSkippingIndexITSuite.scala
+++ integ-test/src/test/scala/org/opensearch/flint/spark/iceberg/FlintSparkIcebergSkippingIndexITSuite.scala
@@ -5,8 +5,12 @@
 
 package org.opensearch.flint.spark.iceberg
 
+import org.junit.Ignore
 import org.opensearch.flint.spark.FlintSparkSkippingIndexSqlITSuite
 
+// FIXME: https://github.com/opensearch-project/opensearch-spark/issues/331#issuecomment-2110948494
+/*
 class FlintSparkIcebergSkippingIndexITSuite
     extends FlintSparkSkippingIndexSqlITSuite
     with FlintSparkIcebergSuite {}
+ */
