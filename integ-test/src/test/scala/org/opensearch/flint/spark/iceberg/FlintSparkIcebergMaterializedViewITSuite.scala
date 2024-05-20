306d372efdd50f7e6f3a9b6b4817ae5f1a4022f3
Peng Huo <penghuo@gmail.com>
2024-05-20 14:41:17 -0700

    enable Iceberg IT
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- integ-test/src/test/scala/org/opensearch/flint/spark/iceberg/FlintSparkIcebergMaterializedViewITSuite.scala
+++ integ-test/src/test/scala/org/opensearch/flint/spark/iceberg/FlintSparkIcebergMaterializedViewITSuite.scala
@@ -7,9 +7,6 @@ package org.opensearch.flint.spark.iceberg
 
 import org.opensearch.flint.spark.FlintSparkMaterializedViewSqlITSuite
 
-// FIXME: https://github.com/opensearch-project/opensearch-spark/issues/331#issuecomment-2110948494
-/*
 class FlintSparkIcebergMaterializedViewITSuite
     extends FlintSparkMaterializedViewSqlITSuite
     with FlintSparkIcebergSuite {}
- */
