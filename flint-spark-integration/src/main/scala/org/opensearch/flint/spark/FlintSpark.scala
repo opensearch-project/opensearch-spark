689788af6bbc739f30aa5dafbee413b8d629875f
Chen Dai <daichen@amazon.com>
2024-05-31 19:00:03 -0700

    Transition Flint index state to Failed upon refresh job termination (#362)
    * Add await monitor API and IT
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix FlintJob IT
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Add new FlintJob IT
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix failed index cannot be deleted issue
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix FlintJob IT due to multi threading
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    ---------
    
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSpark.scala
+++ flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSpark.scala
@@ -54,7 +54,7 @@ class FlintSpark(val spark: SparkSession) extends Logging {
     spark.conf.getOption("spark.flint.datasource.name").getOrElse("")
 
   /** Flint Spark index monitor */
-  private val flintIndexMonitor: FlintSparkIndexMonitor =
+  val flintIndexMonitor: FlintSparkIndexMonitor =
     new FlintSparkIndexMonitor(spark, flintClient, dataSourceName)
 
   /**
@@ -250,7 +250,8 @@ class FlintSpark(val spark: SparkSession) extends Logging {
       try {
         flintClient
           .startTransaction(indexName, dataSourceName)
-          .initialLog(latest => latest.state == ACTIVE || latest.state == REFRESHING)
+          .initialLog(latest =>
+            latest.state == ACTIVE || latest.state == REFRESHING || latest.state == FAILED)
           .transientLog(latest => latest.copy(state = DELETING))
           .finalLog(latest => latest.copy(state = DELETED))
           .commit(_ => {
