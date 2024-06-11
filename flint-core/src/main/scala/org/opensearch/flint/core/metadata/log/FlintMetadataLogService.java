9faadf4d0baa13984753c5490da0b6d4bdb370da
Sean Kao <seankao@amazon.com>
2024-06-11 13:34:37 -0700

    metadata log service interface
    Signed-off-by: Sean Kao <seankao@amazon.com>

---
+++ flint-core/src/main/scala/org/opensearch/flint/core/metadata/log/FlintMetadataLogService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.flint.core.metadata.log;
+
+import java.util.Optional;
+
+public interface FlintMetadataLogService {
+
+  /**
+   * Start a new optimistic transaction.
+   *
+   * @param indexName index name
+   * @param forceInit forceInit create empty metadata log if not exist.
+   * @return transaction handle
+   */
+  <T> OptimisticTransaction<T> startTransaction(String indexName, boolean forceInit);
+
+  /**
+   * Start a new optimistic transaction.
+   *
+   * @param indexName index name
+   * @return transaction handle
+   */
+  default <T> OptimisticTransaction<T> startTransaction(String indexName) {
+    return startTransaction(indexName, false);
+  }
+
+  /**
+   * Get metadata log for index.
+   *
+   * @param indexName index name
+   * @param forceInit forceInit create empty metadata log if not exist
+   * @return optional metadata log
+   */
+  Optional<FlintMetadataLog<FlintMetadataLogEntry>> getIndexMetadataLog(String indexName, boolean forceInit);
+
+  /**
+   * Get metadata log for index.
+   *
+   * @param indexName index name
+   * @return optional metadata log
+   */
+  default Optional<FlintMetadataLog<FlintMetadataLogEntry>> getIndexMetadataLog(String indexName) {
+    return getIndexMetadataLog(indexName, false);
+  }
+}
