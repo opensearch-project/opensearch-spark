f4e90e4c0e763cf4e5db223c092461d16745046e
Sean Kao <seankao@amazon.com>
2024-06-12 18:46:41 -0700

    undo reflection
    Signed-off-by: Sean Kao <seankao@amazon.com>

--- flint-core/src/main/scala/org/opensearch/flint/core/FlintClientBuilder.java
+++ flint-core/src/main/scala/org/opensearch/flint/core/FlintClientBuilder.java
@@ -5,9 +5,8 @@
 
 package org.opensearch.flint.core;
 
-import java.lang.reflect.Constructor;
 import org.opensearch.flint.core.storage.FlintOpenSearchClient;
-import org.opensearch.flint.core.metadata.log.FlintMetadataLogService;
+import org.opensearch.flint.core.storage.FlintOpenSearchMetadataLogService;
 
 /**
  * {@link FlintClient} builder.
@@ -15,20 +14,6 @@ import org.opensearch.flint.core.metadata.log.FlintMetadataLogService;
 public class FlintClientBuilder {
 
   public static FlintClient build(FlintOptions options) {
-    return new FlintOpenSearchClient(options,
-        instantiateFlintMetadataLogService(options.getCustomFlintMetadataLogServiceClass(), options));
-  }
-
-  /**
-   * Attempts to instantiate Flint metadata log service using reflection.
-   */
-  private static FlintMetadataLogService instantiateFlintMetadataLogService(String className, FlintOptions options) {
-    try {
-      Class<?> flintMetadataLogServiceClass = Class.forName(className);
-      Constructor<?> constructor = flintMetadataLogServiceClass.getConstructor(FlintOptions.class);
-      return (FlintMetadataLogService) constructor.newInstance(options);
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to instantiate FlintMetadataLogService: " + className, e);
-    }
+    return new FlintOpenSearchClient(options, new FlintOpenSearchMetadataLogService(options));
   }
 }
