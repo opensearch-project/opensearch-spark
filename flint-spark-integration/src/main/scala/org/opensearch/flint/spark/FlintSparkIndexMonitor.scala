957f7d3b485876e16db9497ddfb037f9a99f9101
Chen Dai <daichen@amazon.com>
2024-07-15 10:52:01 -0700

    Store root cause message and add IT
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSparkIndexMonitor.scala
+++ flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSparkIndexMonitor.scala
@@ -118,7 +118,7 @@ class FlintSparkIndexMonitor(
       } catch {
         case e: Throwable =>
           logError(s"Streaming job $name terminated with exception: ${e.getMessage}")
-          retryUpdateIndexStateToFailed(name)
+          retryUpdateIndexStateToFailed(name, exception = Some(e))
       }
     } else {
       logInfo(s"Index monitor for [$indexName] not found.")
@@ -132,7 +132,7 @@ class FlintSparkIndexMonitor(
       val name = FlintSparkIndexMonitor.indexMonitorTracker.keys.headOption
       if (name.isDefined) {
         logInfo(s"Found index name in index monitor task list: ${name.get}")
-        retryUpdateIndexStateToFailed(name.get)
+        retryUpdateIndexStateToFailed(name.get, exception = None)
       } else {
         logInfo(s"Index monitor task list is empty")
       }
@@ -195,19 +195,24 @@ class FlintSparkIndexMonitor(
   /**
    * Transition the index state to FAILED upon encountering an exception. Retry in case conflicts
    * with final transaction in scheduled task.
-   * ```
-   * TODO:
-   * 1) Determine the appropriate state code based on the type of exception encountered
-   * 2) Record and persist the error message of the root cause for further diagnostics.
-   * ```
+   *
+   * TODO: Determine the appropriate state code based on the type of exception encountered
    */
-  private def retryUpdateIndexStateToFailed(indexName: String): Unit = {
+  private def retryUpdateIndexStateToFailed(
+      indexName: String,
+      exception: Option[Throwable]): Unit = {
     logInfo(s"Updating index state to failed for $indexName")
     retry {
       flintMetadataLogService
         .startTransaction(indexName)
         .initialLog(latest => latest.state == REFRESHING)
-        .finalLog(latest => latest.copy(state = FAILED))
+        .finalLog(latest =>
+          exception match {
+            case Some(ex) =>
+              latest.copy(state = FAILED, error = extractRootCause(ex))
+            case None =>
+              latest.copy(state = FAILED)
+          })
         .commit(_ => {})
     }
   }
@@ -230,6 +235,21 @@ class FlintSparkIndexMonitor(
       .run(new CheckedRunnable {
         override def run(): Unit = operation
       })
+  }
+
+  private def extractRootCause(e: Throwable): String = {
+    var cause = e
+    while (cause.getCause != null && cause.getCause != cause) {
+      cause = cause.getCause
+    }
+
+    if (cause.getLocalizedMessage != null) {
+      return cause.getLocalizedMessage
+    }
+    if (cause.getMessage != null) {
+      return cause.getMessage
+    }
+    cause.toString
   }
 }
 
