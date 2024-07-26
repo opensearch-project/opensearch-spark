3e4df0a577c9e365ce82a02b195a8ae797a08ae1
Chen Dai <daichen@amazon.com>
2024-07-26 14:32:27 -0700

    Add error output column to show Flint index statement (#436)
    * Add extended in show flint index and refactor AST builder
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Update user manual
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Add IT
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix scalafmt issue for global execution context
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Split ANTLR grammar rule
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Refactor comments and test code
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Remove thread pool in IT
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    ---------
    
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- flint-spark-integration/src/main/scala/org/opensearch/flint/spark/sql/index/FlintSparkIndexAstBuilder.scala
+++ flint-spark-integration/src/main/scala/org/opensearch/flint/spark/sql/index/FlintSparkIndexAstBuilder.scala
@@ -7,12 +7,13 @@ package org.opensearch.flint.spark.sql.index
 
 import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 
+import org.opensearch.flint.spark.FlintSparkIndex
 import org.opensearch.flint.spark.covering.FlintSparkCoveringIndex
 import org.opensearch.flint.spark.mv.FlintSparkMaterializedView
 import org.opensearch.flint.spark.skipping.FlintSparkSkippingIndex
 import org.opensearch.flint.spark.sql.{FlintSparkSqlCommand, FlintSparkSqlExtensionsVisitor, SparkSqlAstBuilder}
 import org.opensearch.flint.spark.sql.FlintSparkSqlAstBuilder.IndexBelongsTo
-import org.opensearch.flint.spark.sql.FlintSparkSqlExtensionsParser.ShowFlintIndexStatementContext
+import org.opensearch.flint.spark.sql.FlintSparkSqlExtensionsParser.{MultipartIdentifierContext, ShowFlintIndexContext, ShowFlintIndexExtendedContext}
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.expressions.AttributeReference
@@ -25,52 +26,123 @@ import org.apache.spark.sql.types.{BooleanType, StringType}
 trait FlintSparkIndexAstBuilder extends FlintSparkSqlExtensionsVisitor[AnyRef] {
   self: SparkSqlAstBuilder =>
 
-  override def visitShowFlintIndexStatement(ctx: ShowFlintIndexStatementContext): Command = {
-    val outputSchema = Seq(
-      AttributeReference("flint_index_name", StringType, nullable = false)(),
-      AttributeReference("kind", StringType, nullable = false)(),
-      AttributeReference("database", StringType, nullable = false)(),
-      AttributeReference("table", StringType, nullable = true)(),
-      AttributeReference("index_name", StringType, nullable = true)(),
-      AttributeReference("auto_refresh", BooleanType, nullable = false)(),
-      AttributeReference("status", StringType, nullable = false)())
+  /**
+   * Represents the basic output schema for the FlintSparkSqlCommand. This schema includes
+   * essential information about each index.
+   */
+  private val baseOutputSchema = Seq(
+    AttributeReference("flint_index_name", StringType, nullable = false)(),
+    AttributeReference("kind", StringType, nullable = false)(),
+    AttributeReference("database", StringType, nullable = false)(),
+    AttributeReference("table", StringType, nullable = true)(),
+    AttributeReference("index_name", StringType, nullable = true)(),
+    AttributeReference("auto_refresh", BooleanType, nullable = false)(),
+    AttributeReference("status", StringType, nullable = false)())
 
-    FlintSparkSqlCommand(outputSchema) { flint =>
-      val catalogDbName =
-        ctx.catalogDb.parts
-          .map(part => part.getText)
-          .mkString("_")
-      val indexNamePattern = s"flint_${catalogDbName}_*"
-      flint
-        .describeIndexes(indexNamePattern)
-        .filter(index => index belongsTo ctx.catalogDb)
-        .map { index =>
-          val (databaseName, tableName, indexName) = index match {
-            case skipping: FlintSparkSkippingIndex =>
-              val parts = skipping.tableName.split('.')
-              (parts(1), parts.drop(2).mkString("."), null)
-            case covering: FlintSparkCoveringIndex =>
-              val parts = covering.tableName.split('.')
-              (parts(1), parts.drop(2).mkString("."), covering.indexName)
-            case mv: FlintSparkMaterializedView =>
-              val parts = mv.mvName.split('.')
-              (parts(1), null, parts.drop(2).mkString("."))
-          }
+  /**
+   * Extends the base output schema with additional information. This schema is used when the
+   * EXTENDED keyword is present.
+   */
+  private val extendedOutputSchema = Seq(
+    AttributeReference("error", StringType, nullable = true)())
 
-          val status = index.latestLogEntry match {
-            case Some(entry) => entry.state.toString
-            case None => "unavailable"
-          }
+  override def visitShowFlintIndex(ctx: ShowFlintIndexContext): Command = {
+    new ShowFlintIndexCommandBuilder()
+      .withSchema(baseOutputSchema)
+      .forCatalog(ctx.catalogDb)
+      .constructRows(baseRowData)
+      .build()
+  }
 
-          Row(
-            index.name,
-            index.kind,
-            databaseName,
-            tableName,
-            indexName,
-            index.options.autoRefresh(),
-            status)
-        }
+  override def visitShowFlintIndexExtended(ctx: ShowFlintIndexExtendedContext): Command = {
+    new ShowFlintIndexCommandBuilder()
+      .withSchema(baseOutputSchema ++ extendedOutputSchema)
+      .forCatalog(ctx.catalogDb)
+      .constructRows(index => baseRowData(index) ++ extendedRowData(index))
+      .build()
+  }
+
+  /**
+   * Builder class for constructing FlintSparkSqlCommand objects.
+   */
+  private class ShowFlintIndexCommandBuilder {
+    private var schema: Seq[AttributeReference] = _
+    private var catalogDb: MultipartIdentifierContext = _
+    private var rowDataBuilder: FlintSparkIndex => Seq[Any] = _
+
+    /** Specify the output schema for the command. */
+    def withSchema(schema: Seq[AttributeReference]): ShowFlintIndexCommandBuilder = {
+      this.schema = schema
+      this
     }
+
+    /** Specify the catalog database context for the command. */
+    def forCatalog(catalogDb: MultipartIdentifierContext): ShowFlintIndexCommandBuilder = {
+      this.catalogDb = catalogDb
+      this
+    }
+
+    /** Configures a function to construct row data for each index. */
+    def constructRows(
+        rowDataBuilder: FlintSparkIndex => Seq[Any]): ShowFlintIndexCommandBuilder = {
+      this.rowDataBuilder = rowDataBuilder
+      this
+    }
+
+    /** Builds the command using the configured parameters. */
+    def build(): FlintSparkSqlCommand = {
+      require(schema != null, "Schema must be set before building the command")
+      require(catalogDb != null, "Catalog database must be set before building the command")
+      require(rowDataBuilder != null, "Row data builder must be set before building the command")
+
+      FlintSparkSqlCommand(schema) { flint =>
+        val catalogDbName =
+          catalogDb.parts
+            .map(part => part.getText)
+            .mkString("_")
+        val indexNamePattern = s"flint_${catalogDbName}_*"
+
+        flint
+          .describeIndexes(indexNamePattern)
+          .filter(index => index belongsTo catalogDb)
+          .map { index => Row.fromSeq(rowDataBuilder(index)) }
+      }
+    }
+  }
+
+  private def baseRowData(index: FlintSparkIndex): Seq[Any] = {
+    val (databaseName, tableName, indexName) = index match {
+      case skipping: FlintSparkSkippingIndex =>
+        val parts = skipping.tableName.split('.')
+        (parts(1), parts.drop(2).mkString("."), null)
+      case covering: FlintSparkCoveringIndex =>
+        val parts = covering.tableName.split('.')
+        (parts(1), parts.drop(2).mkString("."), covering.indexName)
+      case mv: FlintSparkMaterializedView =>
+        val parts = mv.mvName.split('.')
+        (parts(1), null, parts.drop(2).mkString("."))
+    }
+
+    val status = index.latestLogEntry match {
+      case Some(entry) => entry.state.toString
+      case None => "unavailable"
+    }
+
+    Seq(
+      index.name,
+      index.kind,
+      databaseName,
+      tableName,
+      indexName,
+      index.options.autoRefresh(),
+      status)
+  }
+
+  private def extendedRowData(index: FlintSparkIndex): Seq[Any] = {
+    val error = index.latestLogEntry match {
+      case Some(entry) => entry.error
+      case None => null
+    }
+    Seq(error)
   }
 }
