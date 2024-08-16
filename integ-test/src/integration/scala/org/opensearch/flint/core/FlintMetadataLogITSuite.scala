857622457c4f964324bafa780e5ea793b27a722e
Tomoyuki Morita <moritato@amazon.com>
2024-08-16 09:10:40 -0700

    Revert unrelated change
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- integ-test/src/integration/scala/org/opensearch/flint/core/FlintMetadataLogITSuite.scala
+++ integ-test/src/integration/scala/org/opensearch/flint/core/FlintMetadataLogITSuite.scala
@@ -46,7 +46,7 @@ class FlintMetadataLogITSuite extends OpenSearchTransactionSuite with Matchers {
 
   test("should build metadata log service") {
     val customOptions =
-      openSearchOptions + (FlintOptions.CUSTOM_FLINT_METADATA_LOG_SERVICE_CLASS -> "org.opensearch.flint.core.TestMetadataLogService")
+      openSearchOptions + (CUSTOM_FLINT_METADATA_LOG_SERVICE_CLASS.key -> "org.opensearch.flint.core.TestMetadataLogService")
     val customFlintOptions = new FlintOptions(customOptions.asJava)
     val customFlintMetadataLogService =
       FlintMetadataLogServiceBuilder.build(customFlintOptions, sparkConf)
@@ -54,8 +54,7 @@ class FlintMetadataLogITSuite extends OpenSearchTransactionSuite with Matchers {
   }
 
   test("should fail to build metadata log service if class name doesn't exist") {
-    val options =
-      openSearchOptions + (FlintOptions.CUSTOM_FLINT_METADATA_LOG_SERVICE_CLASS -> "dummy")
+    val options = openSearchOptions + (CUSTOM_FLINT_METADATA_LOG_SERVICE_CLASS.key -> "dummy")
     val flintOptions = new FlintOptions(options.asJava)
     the[RuntimeException] thrownBy {
       FlintMetadataLogServiceBuilder.build(flintOptions, sparkConf)
