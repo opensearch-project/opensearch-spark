05c17942e1f860c4aa99838d28258c5530bebc36
Lantao Jin <ltjin@amazon.com>
2024-08-31 00:07:19 +0800

    Translate PPL `dedup` Command Part 2:  allowedDuplication>1 (#543)
    * Translate PPL  Command Part 2:  allowedDuplication>1
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * update document
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * remove the CatalystQueryPlanVisitor.java.orig
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * refactor
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * add IT for reordering field list
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * remove useless code
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    ---------
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    Co-authored-by: YANGDB <yang.db.dev@gmail.com>
    (cherry picked from commit cc52b1c3e68b87dfb735c3fcd055b5e93d015720)

--- ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanDedupeTranslatorTestSuite.scala
+++ ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanDedupeTranslatorTestSuite.scala
@@ -7,15 +7,16 @@ package org.opensearch.flint.spark.ppl
 
 import org.opensearch.flint.spark.ppl.PlaneUtils.plan
 import org.opensearch.sql.ppl.{CatalystPlanContext, CatalystQueryPlanVisitor}
+import org.opensearch.sql.ppl.utils.{SortUtils, WindowSpecTransformer}
 import org.scalatest.matchers.should.Matchers
 
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.sql.catalyst.analysis.{UnresolvedAttribute, UnresolvedRelation, UnresolvedStar}
-import org.apache.spark.sql.catalyst.expressions.{And, IsNotNull, IsNull, NamedExpression, Or}
+import org.apache.spark.sql.catalyst.analysis.{UnresolvedAttribute, UnresolvedRelation}
+import org.apache.spark.sql.catalyst.expressions.{Alias, And, Ascending, CurrentRow, IsNotNull, IsNull, LessThanOrEqual, Literal, NamedExpression, Or, RowFrame, RowNumber, SortOrder, SpecifiedWindowFrame, UnboundedPreceding, WindowExpression, WindowSpecDefinition}
 import org.apache.spark.sql.catalyst.plans.PlanTest
-import org.apache.spark.sql.catalyst.plans.logical.{Deduplicate, Filter, Project, Union}
+import org.apache.spark.sql.catalyst.plans.logical.{DataFrameDropColumns, Deduplicate, Filter, Project, Union, Window}
 
-class PPLLogicalPlanDedupTranslatorTestSuite
+class PPLLogicalPlanDedupeTranslatorTestSuite
     extends SparkFunSuite
     with PlanTest
     with LogicalPlanTestUtils
@@ -229,40 +230,164 @@ class PPLLogicalPlanDedupTranslatorTestSuite
     assert(ex.getMessage === "Number of duplicate events must be greater than 0")
   }
 
-  // Todo
-  ignore("test dedup 2 a") {
+  test("test dedup 2 a") {
     val context = new CatalystPlanContext
     val logPlan = planTransformer.visit(
       plan(pplParser, "source=table | dedup 2 a | fields a", false),
       context)
 
+    val projectList: Seq[NamedExpression] = Seq(UnresolvedAttribute("a"))
+    val isNotNullFilter =
+      Filter(IsNotNull(UnresolvedAttribute("a")), UnresolvedRelation(Seq("table")))
+    val windowExpression = WindowExpression(
+      RowNumber(),
+      WindowSpecDefinition(
+        UnresolvedAttribute("a") :: Nil,
+        SortOrder(UnresolvedAttribute("a"), Ascending) :: Nil,
+        SpecifiedWindowFrame(RowFrame, UnboundedPreceding, CurrentRow)))
+    val rowNumberAlias = Alias(windowExpression, WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)()
+    val partitionSpec = projectList
+    val orderSpec = Seq(SortUtils.sortOrder(UnresolvedAttribute("a"), true))
+    val window = Window(Seq(rowNumberAlias), partitionSpec, orderSpec, isNotNullFilter)
+    val deduplicateFilter =
+      Filter(
+        LessThanOrEqual(
+          UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME),
+          Literal(2)),
+        window)
+    val dropColumns =
+      DataFrameDropColumns(
+        Seq(UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)),
+        deduplicateFilter)
+    val expectedPlan = Project(projectList, dropColumns)
+    comparePlans(expectedPlan, logPlan, checkAnalysis = false)
   }
 
-  // Todo
-  ignore("test dedup 2 a, b, c") {
+  test("test dedup 2 a, b, c") {
     val context = new CatalystPlanContext
     val logPlan = planTransformer.visit(
       plan(pplParser, "source=table | dedup 2 a, b, c | fields a, b, c", false),
       context)
 
+    val projectList: Seq[NamedExpression] =
+      Seq(UnresolvedAttribute("a"), UnresolvedAttribute("b"), UnresolvedAttribute("c"))
+    val isNotNullFilter = Filter(
+      And(
+        And(IsNotNull(UnresolvedAttribute("a")), IsNotNull(UnresolvedAttribute("b"))),
+        IsNotNull(UnresolvedAttribute("c"))),
+      UnresolvedRelation(Seq("table")))
+    val windowExpression = WindowExpression(
+      RowNumber(),
+      WindowSpecDefinition(
+        UnresolvedAttribute("a") :: UnresolvedAttribute("b") :: UnresolvedAttribute("c") :: Nil,
+        SortOrder(UnresolvedAttribute("a"), Ascending) :: SortOrder(
+          UnresolvedAttribute("b"),
+          Ascending) :: SortOrder(UnresolvedAttribute("c"), Ascending) :: Nil,
+        SpecifiedWindowFrame(RowFrame, UnboundedPreceding, CurrentRow)))
+    val rowNumberAlias = Alias(windowExpression, WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)()
+    val partitionSpec = projectList
+    val orderSpec = Seq(
+      SortUtils.sortOrder(UnresolvedAttribute("a"), true),
+      SortUtils.sortOrder(UnresolvedAttribute("b"), true),
+      SortUtils.sortOrder(UnresolvedAttribute("c"), true))
+    val window = Window(Seq(rowNumberAlias), partitionSpec, orderSpec, isNotNullFilter)
+    val deduplicateFilter =
+      Filter(
+        LessThanOrEqual(
+          UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME),
+          Literal(2)),
+        window)
+    val dropColumns =
+      DataFrameDropColumns(
+        Seq(UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)),
+        deduplicateFilter)
+    val expectedPlan = Project(projectList, dropColumns)
+    comparePlans(expectedPlan, logPlan, checkAnalysis = false)
   }
 
-  // Todo
-  ignore("test dedup 2 a keepempty=true") {
+  test("test dedup 2 a keepempty=true") {
     val context = new CatalystPlanContext
     val logPlan = planTransformer.visit(
       plan(pplParser, "source=table | dedup 2 a keepempty=true | fields a", false),
       context)
 
+    val projectList: Seq[NamedExpression] = Seq(UnresolvedAttribute("a"))
+    val isNotNullFilter =
+      Filter(IsNotNull(UnresolvedAttribute("a")), UnresolvedRelation(Seq("table")))
+    val windowExpression = WindowExpression(
+      RowNumber(),
+      WindowSpecDefinition(
+        UnresolvedAttribute("a") :: Nil,
+        SortOrder(UnresolvedAttribute("a"), Ascending) :: Nil,
+        SpecifiedWindowFrame(RowFrame, UnboundedPreceding, CurrentRow)))
+    val rowNumberAlias = Alias(windowExpression, WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)()
+    val partitionSpec = projectList
+    val orderSpec = Seq(SortUtils.sortOrder(UnresolvedAttribute("a"), true))
+    val window = Window(Seq(rowNumberAlias), partitionSpec, orderSpec, isNotNullFilter)
+    val deduplicateFilter =
+      Filter(
+        LessThanOrEqual(
+          UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME),
+          Literal(2)),
+        window)
+    val dropColumns =
+      DataFrameDropColumns(
+        Seq(UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)),
+        deduplicateFilter)
+
+    val isNullFilter = Filter(IsNull(UnresolvedAttribute("a")), UnresolvedRelation(Seq("table")))
+    val union = Union(dropColumns, isNullFilter)
+    val expectedPlan = Project(projectList, union)
+    comparePlans(expectedPlan, logPlan, checkAnalysis = false)
   }
 
-  // Todo
-  ignore("test dedup 2 a, b, c keepempty=true") {
+  test("test dedup 2 a, b, c keepempty=true") {
     val context = new CatalystPlanContext
     val logPlan = planTransformer.visit(
       plan(pplParser, "source=table | dedup 2 a, b, c keepempty=true | fields a, b, c", false),
       context)
 
+    val projectList: Seq[NamedExpression] =
+      Seq(UnresolvedAttribute("a"), UnresolvedAttribute("b"), UnresolvedAttribute("c"))
+    val isNotNullFilter = Filter(
+      And(
+        And(IsNotNull(UnresolvedAttribute("a")), IsNotNull(UnresolvedAttribute("b"))),
+        IsNotNull(UnresolvedAttribute("c"))),
+      UnresolvedRelation(Seq("table")))
+    val windowExpression = WindowExpression(
+      RowNumber(),
+      WindowSpecDefinition(
+        UnresolvedAttribute("a") :: UnresolvedAttribute("b") :: UnresolvedAttribute("c") :: Nil,
+        SortOrder(UnresolvedAttribute("a"), Ascending) :: SortOrder(
+          UnresolvedAttribute("b"),
+          Ascending) :: SortOrder(UnresolvedAttribute("c"), Ascending) :: Nil,
+        SpecifiedWindowFrame(RowFrame, UnboundedPreceding, CurrentRow)))
+    val rowNumberAlias = Alias(windowExpression, WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)()
+    val partitionSpec = projectList
+    val orderSpec = Seq(
+      SortUtils.sortOrder(UnresolvedAttribute("a"), true),
+      SortUtils.sortOrder(UnresolvedAttribute("b"), true),
+      SortUtils.sortOrder(UnresolvedAttribute("c"), true))
+    val window = Window(Seq(rowNumberAlias), partitionSpec, orderSpec, isNotNullFilter)
+    val deduplicateFilter =
+      Filter(
+        LessThanOrEqual(
+          UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME),
+          Literal(2)),
+        window)
+    val dropColumns =
+      DataFrameDropColumns(
+        Seq(UnresolvedAttribute(WindowSpecTransformer.ROW_NUMBER_COLUMN_NAME)),
+        deduplicateFilter)
+
+    val isNullFilter = Filter(
+      Or(
+        Or(IsNull(UnresolvedAttribute("a")), IsNull(UnresolvedAttribute("b"))),
+        IsNull(UnresolvedAttribute("c"))),
+      UnresolvedRelation(Seq("table")))
+    val union = Union(dropColumns, isNullFilter)
+    val expectedPlan = Project(projectList, union)
+    comparePlans(expectedPlan, logPlan, checkAnalysis = false)
   }
 
   test("test dedup 2 a consecutive=true") {
