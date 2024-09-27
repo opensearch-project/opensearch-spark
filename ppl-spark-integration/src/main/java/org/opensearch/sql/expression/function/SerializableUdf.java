b82407bceffbea9ed0957aaafd172c326fb4e20e
Hendrik Saly <hendrik.saly@eliatra.com>
2024-09-27 11:43:13 +0200

    Add inet.ipaddr library and implement basic cidr check
    Signed-off-by: Hendrik Saly <hendrik.saly@eliatra.com>

--- ppl-spark-integration/src/main/java/org/opensearch/sql/expression/function/SerializableUdf.java
+++ ppl-spark-integration/src/main/java/org/opensearch/sql/expression/function/SerializableUdf.java
@@ -5,6 +5,9 @@
 
 package org.opensearch.sql.expression.function;
 
+import inet.ipaddr.AddressStringException;
+import inet.ipaddr.IPAddressString;
+import inet.ipaddr.IPAddressStringParameters;
 import scala.Function2;
 import scala.Serializable;
 import scala.runtime.AbstractFunction2;
@@ -20,10 +23,33 @@ public class SerializableUdf {
 
     public static Function2<String,String,Boolean> cidrFunction = new SerializableAbstractFunction2<>() {
 
+        IPAddressStringParameters valOptions = new IPAddressStringParameters.Builder()
+                .allowEmpty(false)
+                .setEmptyAsLoopback(false)
+                .allow_inet_aton(false)
+                .allowSingleSegment(false)
+                .toParams();
+
         @Override
         public Boolean apply(String ipAddress, String cidrBlock) {
-            //TODO implement ip address validation and cidr validation for ipv4 and ipv6
-            return Boolean.FALSE;
+
+            IPAddressString parsedIpAddress = new IPAddressString(ipAddress, valOptions);
+
+            try {
+                parsedIpAddress.validate();
+            } catch (AddressStringException e) {
+                throw new RuntimeException("The given ipAddress '"+ipAddress+"' is invalid. It must be a valid IPv4 or IPv6 address. Error details: "+e.getMessage());
+            }
+
+            IPAddressString parsedCidrBlock = new IPAddressString(cidrBlock, valOptions);
+
+            try {
+                parsedCidrBlock.validate();
+            } catch (AddressStringException e) {
+                throw new RuntimeException("The given cidrBlock '"+cidrBlock+"' is invalid. It must be a valid CIDR or netmask. Error details: "+e.getMessage());
+            }
+
+            return parsedCidrBlock.contains(parsedIpAddress);
         }
     };
 
