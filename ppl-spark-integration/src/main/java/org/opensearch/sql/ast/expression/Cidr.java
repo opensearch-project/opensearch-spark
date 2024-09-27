7bc96c8bf84d0bf59f82712d2c663e5b5d235d88
Hendrik Saly <hendrik.saly@eliatra.com>
2024-09-27 11:49:27 +0200

    Refactor for using lombok
    Signed-off-by: Hendrik Saly <hendrik.saly@eliatra.com>

--- ppl-spark-integration/src/main/java/org/opensearch/sql/ast/expression/Cidr.java
+++ ppl-spark-integration/src/main/java/org/opensearch/sql/ast/expression/Cidr.java
@@ -5,27 +5,23 @@
 
 package org.opensearch.sql.ast.expression;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import org.opensearch.sql.ast.AbstractNodeVisitor;
 
 import java.util.Arrays;
 import java.util.List;
 
+/** AST node that represents CIDR function. */
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(callSuper = false)
+@ToString
 public class Cidr extends UnresolvedExpression {
     private UnresolvedExpression ipAddress;
     private UnresolvedExpression cidrBlock;
-
-    public Cidr(UnresolvedExpression ipAddress, UnresolvedExpression cidrBlock) {
-        this.ipAddress = ipAddress;
-        this.cidrBlock = cidrBlock;
-    }
-
-    public UnresolvedExpression getCidrBlock() {
-        return cidrBlock;
-    }
-
-    public UnresolvedExpression getIpAddress() {
-        return ipAddress;
-    }
 
     @Override
     public List<UnresolvedExpression> getChild() {
@@ -35,12 +31,5 @@ public class Cidr extends UnresolvedExpression {
     @Override
     public <T, C> T accept(AbstractNodeVisitor<T, C> nodeVisitor, C context) {
         return nodeVisitor.visitCidr(this, context);
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "CIDR(%s,%s)",
-                ipAddress.toString(), cidrBlock.toString());
     }
 }
