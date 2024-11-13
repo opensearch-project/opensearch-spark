cbcab2ff8578c58f3fa529b7e9fc438eb89f5e04
Tomoyuki MORITA <moritato@amazon.com>
2024-11-13 11:24:15 -0800

    Add jvmGCTime metrics (#889)
    * Add jvmGCTime metrics
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix style
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- flint-core/src/main/scala/org/opensearch/flint/core/metrics/MetricsSparkListener.scala
+++ flint-core/src/main/scala/org/opensearch/flint/core/metrics/MetricsSparkListener.scala
@@ -6,17 +6,18 @@
 package org.opensearch.flint.core.metrics
 
 import org.apache.spark.internal.Logging
-import org.apache.spark.scheduler.{SparkListener, SparkListenerTaskEnd}
+import org.apache.spark.scheduler.{SparkListener, SparkListenerExecutorMetricsUpdate, SparkListenerTaskEnd}
 import org.apache.spark.sql.SparkSession
 
 /**
- * Collect and emit bytesRead/Written and recordsRead/Written metrics
+ * Collect and emit metrics by listening spark events
  */
-class ReadWriteBytesSparkListener extends SparkListener with Logging {
+class MetricsSparkListener extends SparkListener with Logging {
   var bytesRead: Long = 0
   var recordsRead: Long = 0
   var bytesWritten: Long = 0
   var recordsWritten: Long = 0
+  var totalJvmGcTime: Long = 0
 
   override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
     val inputMetrics = taskEnd.taskMetrics.inputMetrics
@@ -31,21 +32,28 @@ class ReadWriteBytesSparkListener extends SparkListener with Logging {
     recordsRead += inputMetrics.recordsRead
     bytesWritten += outputMetrics.bytesWritten
     recordsWritten += outputMetrics.recordsWritten
+    totalJvmGcTime += taskEnd.taskMetrics.jvmGCTime
+
+    MetricsUtil.addHistoricGauge(
+      MetricConstants.TASK_JVM_GC_TIME_METRIC,
+      taskEnd.taskMetrics.jvmGCTime)
   }
 
   def emitMetrics(): Unit = {
     logInfo(s"Input: totalBytesRead=${bytesRead}, totalRecordsRead=${recordsRead}")
     logInfo(s"Output: totalBytesWritten=${bytesWritten}, totalRecordsWritten=${recordsWritten}")
+    logInfo(s"totalJvmGcTime=${totalJvmGcTime}")
     MetricsUtil.addHistoricGauge(MetricConstants.INPUT_TOTAL_BYTES_READ, bytesRead)
     MetricsUtil.addHistoricGauge(MetricConstants.INPUT_TOTAL_RECORDS_READ, recordsRead)
     MetricsUtil.addHistoricGauge(MetricConstants.OUTPUT_TOTAL_BYTES_WRITTEN, bytesWritten)
     MetricsUtil.addHistoricGauge(MetricConstants.OUTPUT_TOTAL_RECORDS_WRITTEN, recordsWritten)
+    MetricsUtil.addHistoricGauge(MetricConstants.TOTAL_JVM_GC_TIME_METRIC, totalJvmGcTime)
   }
 }
 
-object ReadWriteBytesSparkListener {
+object MetricsSparkListener {
   def withMetrics[T](spark: SparkSession, lambda: () => T): T = {
-    val listener = new ReadWriteBytesSparkListener()
+    val listener = new MetricsSparkListener()
     spark.sparkContext.addSparkListener(listener)
 
     val result = lambda()
