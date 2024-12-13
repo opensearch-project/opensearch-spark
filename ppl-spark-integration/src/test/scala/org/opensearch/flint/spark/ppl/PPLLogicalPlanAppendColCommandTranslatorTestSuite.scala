696b98ef9ce651836d7ddbd61afee3f11024f0de
Andy Kwok <andy.kwok@improving.com>
2024-12-12 16:25:33 -0800

    Test-cases 3 and 4
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanAppendColCommandTranslatorTestSuite.scala
+++ ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanAppendColCommandTranslatorTestSuite.scala
@@ -171,14 +171,155 @@ class PPLLogicalPlanAppendColCommandTranslatorTestSuite
       DataFrameDropColumns(T12_COLUMNS_SEQ,
         Join(t1, t2, LeftOuter, Some(T12_JOIN_CONDITION), JoinHint.NONE)))
 
-    // scalastyle:off
-    println(logicalPlan)
-    println(result)
-
     comparePlans(logicalPlan, result, checkAnalysis = false)
-
   }
 
+  /**
+   * 'Project [*]
+   * +- 'DataFrameDropColumns ['T1._row_number_, 'T2._row_number_]
+   * +- 'Join LeftOuter, ('T1._row_number_ = 'T2._row_number_)
+   * :- 'SubqueryAlias T1
+   * :  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#427, *]
+   * :     +- 'Project ['age, 'dept, 'salary]
+   * :        +- 'UnresolvedRelation [employees], [], false
+   * +- 'SubqueryAlias T2
+   *  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#432, *]
+   *    +- 'DataFrameDropColumns ['m]
+   *      +- 'Project [*, 1 AS m#430]
+   *        +- 'Aggregate ['age AS age#429], ['COUNT(*) AS count()#428, 'age AS age#429]
+   *          +- 'UnresolvedRelation [employees], [], false
+   */
+  test("test AppendCol with chained sub-search") {
+    val context = new CatalystPlanContext
+    val logicalPlan = planTransformer.visit(
+      plan(pplParser, "source=employees | FIELDS age, dept, salary | APPENDCOL [ stats count() by age | eval m = 1 | FIELDS -m ];"),
+      context
+    )
 
+    /*
+      :- 'SubqueryAlias T1
+      :  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST,
+                specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#11, *]
+      :     +- 'Project ['age, 'dept, 'salary]
+      :        +- 'UnresolvedRelation [relation], [], false
+     */
+    val t1 = SubqueryAlias("T1", Project(
+      Seq(ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      Project(Seq(
+        UnresolvedAttribute("age"),
+        UnresolvedAttribute("dept"),
+        UnresolvedAttribute("salary")), RELATION_EMPLOYEES)))
+
+
+    /*
+    +- 'SubqueryAlias T2
+     +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#432, *]
+        +- 'DataFrameDropColumns ['m]
+           +- 'Project [*, 1 AS m#430]
+              +- 'Aggregate ['age AS age#429], ['COUNT(*) AS count()#428, 'age AS age#429]
+                 +- 'UnresolvedRelation [employees], [], false
+     */
+    val t2 = SubqueryAlias("T2", Project(
+      Seq(ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      DataFrameDropColumns(Seq(UnresolvedAttribute("m")),
+        Project(Seq(UnresolvedStar(None), Alias(Literal(1),"m")()),
+          Aggregate(
+            AGE_ALIAS :: Nil, Seq(COUNT_STAR, AGE_ALIAS),
+            RELATION_EMPLOYEES)))
+    ))
+
+    val result = Project(Seq(UnresolvedStar(None)),
+      DataFrameDropColumns(T12_COLUMNS_SEQ,
+        Join(t1, t2, LeftOuter, Some(T12_JOIN_CONDITION), JoinHint.NONE)))
+
+    comparePlans(logicalPlan, result, checkAnalysis = false)
+  }
+
+  /**
+   * == Parsed Logical Plan ==
+   * 'Project [*]
+   * +- 'DataFrameDropColumns ['T1._row_number_, 'T2._row_number_]
+   * +- 'Join LeftOuter, ('T1._row_number_ = 'T2._row_number_)
+   * :- 'SubqueryAlias T1
+   * :  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#551, *]
+   * :     +- 'DataFrameDropColumns ['T1._row_number_, 'T2._row_number_]
+   * :        +- 'Join LeftOuter, ('T1._row_number_ = 'T2._row_number_)
+   * :           :- 'SubqueryAlias T1
+   * :           :  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#544, *]
+   * :           :     +- 'Project ['name, 'age]
+   * :           :        +- 'UnresolvedRelation [employees], [], false
+   * :           +- 'SubqueryAlias T2
+   * :              +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#549, *]
+   * :                 +- 'DataFrameDropColumns ['m]
+   * :                    +- 'Project [*, 1 AS m#547]
+   * :                       +- 'Aggregate ['age AS age#546], ['COUNT(*) AS count()#545, 'age AS age#546]
+   * :                          +- 'UnresolvedRelation [employees], [], false
+   * +- 'SubqueryAlias T2
+   *  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#553, *]
+   *    +- 'Project ['dept]
+   *      +- 'UnresolvedRelation [employees], [], false
+   */
+  test("test multiple AppendCol clauses") {
+    val context = new CatalystPlanContext
+    val logicalPlan = planTransformer.visit(
+      plan(pplParser, "source=employees | FIELDS name, age | APPENDCOL [ stats count() by age | eval m = 1 | FIELDS -m ] | APPENDCOL [FIELDS dept];"),
+      context
+    )
+
+    /*
+      :- 'SubqueryAlias T1
+      :  +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#544, *]
+      :     +- 'Project ['name, 'age]
+      :        +- 'UnresolvedRelation [employees], [], false
+     */
+    val mainSearch = SubqueryAlias("T1", Project(
+      Seq(ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      Project(Seq(
+        UnresolvedAttribute("name"),
+        UnresolvedAttribute("age")), RELATION_EMPLOYEES)))
+
+
+    /*
+    +- 'SubqueryAlias T2
+     +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#432, *]
+        +- 'DataFrameDropColumns ['m]
+           +- 'Project [*, 1 AS m#430]
+              +- 'Aggregate ['age AS age#429], ['COUNT(*) AS count()#428, 'age AS age#429]
+                 +- 'UnresolvedRelation [employees], [], false
+     */
+    val firstAppenCol = SubqueryAlias("T2", Project(
+      Seq(ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      DataFrameDropColumns(Seq(UnresolvedAttribute("m")),
+        Project(Seq(UnresolvedStar(None), Alias(Literal(1),"m")()),
+          Aggregate(
+            AGE_ALIAS :: Nil, Seq(COUNT_STAR, AGE_ALIAS),
+            RELATION_EMPLOYEES)))
+    ))
+
+
+    val joinWithFirstAppendCol = SubqueryAlias("T1", Project(Seq(
+        ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      DataFrameDropColumns(T12_COLUMNS_SEQ,
+        Join(mainSearch, firstAppenCol, LeftOuter, Some(T12_JOIN_CONDITION), JoinHint.NONE))))
+
+
+    /*
+    +- 'SubqueryAlias T2
+     +- 'Project [row_number() windowspecdefinition(1 DESC NULLS LAST, specifiedwindowframe(RowFrame, unboundedpreceding$(), currentrow$())) AS _row_number_#553, *]
+        +- 'Project ['dept]
+           +- 'UnresolvedRelation [employees], [], false
+     */
+    val secondAppendCol = SubqueryAlias("T2", Project(
+      Seq(ROW_NUMBER_AGGREGATION, UnresolvedStar(None)),
+      Project(Seq(
+        UnresolvedAttribute("dept")), RELATION_EMPLOYEES)))
+
+
+    val joinWithSecondAppendCol = Project(Seq(UnresolvedStar(None)),
+      DataFrameDropColumns(T12_COLUMNS_SEQ,
+        Join(joinWithFirstAppendCol, secondAppendCol, LeftOuter, Some(T12_JOIN_CONDITION), JoinHint.NONE)))
+
+    comparePlans(logicalPlan, joinWithSecondAppendCol, checkAnalysis = false)
+  }
 
 }
