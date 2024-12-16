0ffad0e26dfbaca438fe56ae21febd8394e8029d
Chen Dai <daichen@amazon.com>
2024-12-16 13:03:14 -0800

    Add IT for WAF intergation
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- integ-test/src/integration/scala/org/opensearch/flint/spark/FlintSparkMaterializedViewIntegrationsITSuite.scala
+++ integ-test/src/integration/scala/org/opensearch/flint/spark/FlintSparkMaterializedViewIntegrationsITSuite.scala
@@ -19,7 +19,7 @@ import org.apache.spark.sql.flint.FlintDataSourceV2.FLINT_DATASOURCE
 
 class FlintSparkMaterializedViewIntegrationsITSuite extends FlintSparkSuite with Matchers {
 
-  test("create materialized view for VPC flow integration") {
+  test("create aggregated materialized view for VPC flow integration") {
     withIntegration("vpc_flow") { integration =>
       integration
         .createSourceTable(s"$catalogName.default.vpc_low_test")
@@ -83,7 +83,7 @@ class FlintSparkMaterializedViewIntegrationsITSuite extends FlintSparkSuite with
     }
   }
 
-  test("create materialized view for CloudTrail integration") {
+  test("create aggregated materialized view for CloudTrail integration") {
     withIntegration("cloud_trail") { integration =>
       integration
         .createSourceTable(s"$catalogName.default.cloud_trail_test")
@@ -142,6 +142,69 @@ class FlintSparkMaterializedViewIntegrationsITSuite extends FlintSparkSuite with
           "sts.amazonaws.com",
           "AssumeRole",
           "Management",
+          1))
+    }
+  }
+
+  test("create aggregated materialized view for WAF integration") {
+    withIntegration("waf") { integration =>
+      integration
+        .createSourceTable(s"$catalogName.default.waf_test")
+        .createMaterializedView(s"""
+             |SELECT
+             |  TUMBLE(`@timestamp`, '5 Minute').start AS `start_time`,
+             |  webaclId AS `aws.waf.webaclId`,
+             |  action AS `aws.waf.action`,
+             |  `httpRequest.clientIp` AS `aws.waf.httpRequest.clientIp`,
+             |  `httpRequest.country` AS `aws.waf.httpRequest.country`,
+             |  `httpRequest.uri` AS `aws.waf.httpRequest.uri`,
+             |  `httpRequest.httpMethod` AS `aws.waf.httpRequest.httpMethod`,
+             |  httpSourceId AS `aws.waf.httpSourceId`,
+             |  terminatingRuleId AS `aws.waf.terminatingRuleId`,
+             |  terminatingRuleType AS `aws.waf.RuleType`,
+             |  `ruleGroupList.ruleId` AS `aws.waf.ruleGroupList.ruleId`,
+             |  COUNT(*) AS `aws.waf.event_count`
+             |FROM (
+             |  SELECT
+             |    CAST(FROM_UNIXTIME(`timestamp`/1000) AS TIMESTAMP) AS `@timestamp`,
+             |    webaclId,
+             |    action,
+             |    httpRequest.clientIp AS `httpRequest.clientIp`,
+             |    httpRequest.country AS `httpRequest.country`,
+             |    httpRequest.uri AS `httpRequest.uri`,
+             |    httpRequest.httpMethod AS `httpRequest.httpMethod`,
+             |    httpSourceId,
+             |    terminatingRuleId,
+             |    terminatingRuleType,
+             |    ruleGroupList.ruleId AS `ruleGroupList.ruleId`
+             |  FROM
+             |    $catalogName.default.waf_test
+             |)
+             |GROUP BY
+             |  TUMBLE(`@timestamp`, '5 Minute'),
+             |  webaclId,
+             |  action,
+             |  `httpRequest.clientIp`,
+             |  `httpRequest.country`,
+             |  `httpRequest.uri`,
+             |  `httpRequest.httpMethod`,
+             |  httpSourceId,
+             |  terminatingRuleId,
+             |  terminatingRuleType,
+             |  `ruleGroupList.ruleId`
+             |""".stripMargin)
+        .assertIndexData(Row(
+          timestampFromUTC("2023-11-01T05:00:00Z"),
+          "webacl-12345",
+          "ALLOW",
+          "192.0.2.1",
+          "US",
+          "/index.html",
+          "GET",
+          "source-1",
+          "rule-1",
+          "REGULAR",
+          Array("group-rule-1"),
           1))
     }
   }
