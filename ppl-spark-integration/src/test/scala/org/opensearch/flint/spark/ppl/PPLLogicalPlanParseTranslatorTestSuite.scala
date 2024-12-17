473f0c3ee28f0a2056e4b16fa02b58793ba256dc
currantw <taylor.curran@improving.com>
2024-12-17 09:19:41 -0800

    Revert `cast` changes.
    Signed-off-by: currantw <taylor.curran@improving.com>

--- ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanParseTranslatorTestSuite.scala
+++ ppl-spark-integration/src/test/scala/org/opensearch/flint/spark/ppl/PPLLogicalPlanParseTranslatorTestSuite.scala
@@ -13,10 +13,9 @@ import org.scalatest.matchers.should.Matchers
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.catalyst.ScalaReflection.universe.Star
 import org.apache.spark.sql.catalyst.analysis.{UnresolvedAttribute, UnresolvedFunction, UnresolvedRelation, UnresolvedStar}
-import org.apache.spark.sql.catalyst.expressions.{Alias, Ascending, Cast, Coalesce, Descending, GreaterThan, Literal, NamedExpression, NullsFirst, NullsLast, RegExpExtract, SortOrder}
+import org.apache.spark.sql.catalyst.expressions.{Alias, Ascending, Coalesce, Descending, GreaterThan, Literal, NamedExpression, NullsFirst, NullsLast, RegExpExtract, SortOrder}
 import org.apache.spark.sql.catalyst.plans.PlanTest
 import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Filter, GlobalLimit, LocalLimit, Project, Sort}
-import org.apache.spark.sql.types.{DataType, IntegerType}
 
 class PPLLogicalPlanParseTranslatorTestSuite
     extends SparkFunSuite
@@ -121,26 +120,24 @@ class PPLLogicalPlanParseTranslatorTestSuite
     assert(compareByString(expectedPlan) === compareByString(logPlan))
   }
 
-  test("test parse street number & address expressions including cast and sort commands") {
+  test("test parse email & host expressions including cast and sort commands") {
     val context = new CatalystPlanContext
     val logPlan =
       planTransformer.visit(
         plan(
           pplParser,
-          "source=t | parse address '(?<streetNumber>\\d+) (?<street>.+)' | where cast(streetNumber as int) > 500 | sort streetNumber | fields streetNumber, street"),
+          "source=t | parse address '(?<streetNumber>\\d+) (?<street>.+)' | where streetNumber > 500 | sort streetNumber | fields streetNumber, street"),
         context)
 
     val addressAttribute = UnresolvedAttribute("address")
     val streetNumberAttribute = UnresolvedAttribute("streetNumber")
     val streetAttribute = UnresolvedAttribute("street")
-
     val streetNumberExpression = Alias(
       RegExpExtract(
         addressAttribute,
         Literal("(?<streetNumber>\\d+) (?<street>.+)"),
         Literal("1")),
       "streetNumber")()
-
     val streetExpression = Alias(
       RegExpExtract(
         addressAttribute,
@@ -148,19 +145,16 @@ class PPLLogicalPlanParseTranslatorTestSuite
         Literal("2")),
       "street")()
 
-    val castExpression = Cast(streetNumberAttribute, IntegerType)
-
     val expectedPlan = Project(
       Seq(streetNumberAttribute, streetAttribute),
       Sort(
         Seq(SortOrder(streetNumberAttribute, Ascending, NullsFirst, Seq.empty)),
         global = true,
         Filter(
-          GreaterThan(castExpression, Literal(500)),
+          GreaterThan(streetNumberAttribute, Literal(500)),
           Project(
             Seq(addressAttribute, streetNumberExpression, streetExpression, UnresolvedStar(None)),
             UnresolvedRelation(Seq("t"))))))
-
     assert(compareByString(expectedPlan) === compareByString(logPlan))
   }
 
