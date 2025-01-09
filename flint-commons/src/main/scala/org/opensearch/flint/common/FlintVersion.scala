6282ceccc23a03d387313ccaa01f778c378aee78
Louis Chu <clingzhi@amazon.com>
2025-01-09 12:11:48 -0800

    Bump Flint version to 0.6.1
    Signed-off-by: Louis Chu <clingzhi@amazon.com>

--- flint-commons/src/main/scala/org/opensearch/flint/common/FlintVersion.scala
+++ flint-commons/src/main/scala/org/opensearch/flint/common/FlintVersion.scala
@@ -20,6 +20,7 @@ object FlintVersion {
   val V_0_4_0: FlintVersion = FlintVersion("0.4.0")
   val V_0_5_0: FlintVersion = FlintVersion("0.5.0")
   val V_0_6_0: FlintVersion = FlintVersion("0.6.0")
+  val V_0_6_1: FlintVersion = FlintVersion("0.6.1")
 
-  def current(): FlintVersion = V_0_6_0
+  def current(): FlintVersion = V_0_6_1
 }
