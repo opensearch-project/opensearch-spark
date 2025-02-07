1a6b9f31bd4486196b77df53138719f1f57df1af
normanj-bitquill <78755797+normanj-bitquill@users.noreply.github.com>
2025-02-07 11:36:53 -0800

    End-to-End Async Query API PPL tests (#1036)
    Signed-off-by: Norman Jordan <norman.jordan@improving.com>

--- e2e-test/src/test/scala/org/opensearch/spark/e2e/SparkTrait.scala
+++ e2e-test/src/test/scala/org/opensearch/spark/e2e/SparkTrait.scala
@@ -10,8 +10,20 @@ import org.apache.spark.sql.SparkSession
 trait SparkTrait {
   var spark : SparkSession = null
 
+  /**
+   * Retrieves the exposed port of Spark Connect on the "spark" container.
+   *
+   * @return Spark Connect port of the "spark" container
+   */
   def getSparkConnectPort(): Int
 
+  /**
+   * Returns an SparkSession object. Constructs a new SparkSession object for use with the integration test docker
+   * cluster "spark" container. Creates a new SparkSession first time this is called, otherwise the existing S3
+   * client is returned.
+   *
+   * @return a SparkSession for use with the integration test docker cluster
+   */
   def getSparkSession(): SparkSession = {
     this.synchronized {
       if (spark == null) {
