7d4bf21db9d42ceaca6d18d60970762feb3cb9c4
Dennis Toepker <toepkerd@amazon.com>
2025-04-24 15:40:30 -0700

    polishing malformed settings and mappings docker image tests
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

--- integ-test/src/integration/scala/org/apache/spark/opensearch/index/OpenSearchIndexITSuite.scala
+++ integ-test/src/integration/scala/org/apache/spark/opensearch/index/OpenSearchIndexITSuite.scala
@@ -10,34 +10,85 @@ import scala.collection.JavaConverters._
 import org.opensearch.flint.core.FlintOptions
 
 import org.apache.spark.opensearch.table.OpenSearchCatalogSuite
-import org.apache.spark.sql.OSClient
+import org.apache.spark.sql.{FlintJob, OSClient}
 
 class OpenSearchIndexITSuite extends OpenSearchCatalogSuite {
 
-  test("Create index with malformed settings should fail") {
-    val osClient = new OSClient(new FlintOptions(openSearchOptions.asJava))
+  var osClient: OSClient = _
+  val indexName = "test_index"
 
-    val mappings = """{
-                     |  "properties": {
-                     |    "accountId": {
-                     |      "type": "keyword"
-                     |    },
-                     |    "eventName": {
-                     |      "type": "keyword"
-                     |    },
-                     |    "eventSource": {
-                     |      "type": "keyword"
-                     |    }
-                     |  }
-                     |}""".stripMargin
+  protected override def beforeAll(): Unit = {
+    super.beforeAll()
+    // initialized after the container is started
+    osClient = new OSClient(new FlintOptions(openSearchOptions.asJava))
+  }
+
+  protected override def afterEach(): Unit = {
+    super.afterEach()
+    deleteTestIndex(indexName)
+  }
+
+  test("FlintJobExecutor creating index with malformed settings should fail") {
+    val mappings =
+      """{
+        |  "properties": {
+        |    "accountId": {
+        |      "type": "keyword"
+        |    },
+        |    "eventName": {
+        |      "type": "keyword"
+        |    },
+        |    "eventSource": {
+        |      "type": "keyword"
+        |    }
+        |  }
+        |}""".stripMargin
 
     // invalid JSON
-    val settings = """{
-                        |  "index": {
-                        |    "refresh_interval": "1s"
-                        |}""".stripMargin
+    val settings =
+      """{
+        |  "index": {
+        |    "refresh_interval": "1s"
+        |}""".stripMargin
 
-    // should throw error
-    osClient.createIndex("test_index", mappings, settings)
+    try {
+      FlintJob.createResultIndex(osClient, indexName, mappings, settings)
+    } catch {
+      case e: Exception =>
+        assert(e.getMessage == s"Failed to get OpenSearch index mapping for $indexName")
+    }
+  }
+
+  test("FlintJobExecutor creating index with malformed mappings should fail") {
+    val osClient = new OSClient(new FlintOptions(openSearchOptions.asJava))
+
+    // invalid JSON
+    val mappings =
+      """{
+        |  "properties": {
+        |    "accountId": {
+        |      "type": "keyword"
+        |    },
+        |    "eventName": {
+        |      "type": "keyword"
+        |    },
+        |    "eventSource": {
+        |      "type": "keyword"
+        |    }
+        |}""".stripMargin
+
+    val settings =
+      """{
+        |  "index": {
+        |    "refresh_interval": "1s"
+        |  }
+        |}""".stripMargin
+
+    try {
+      FlintJob.createResultIndex(osClient, indexName, mappings, settings)
+    } catch {
+      case e: Exception =>
+        assert(e.getMessage == s"Failed to get OpenSearch index mapping for $indexName")
+    }
   }
 }
