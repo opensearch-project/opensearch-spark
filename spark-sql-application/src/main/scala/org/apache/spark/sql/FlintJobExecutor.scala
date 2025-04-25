ed1e5820c15803088381c4b1729fc0cea6dfcdf3
toepkerd <120457569+toepkerd@users.noreply.github.com>
2025-04-25 11:58:03 -0700

    Adding fast refresh setting during index creation (#1074)
    * Adding fast refresh setting during index creation
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * abstracting refresh setting outside of OSclient implementation
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adjusting settings to match that of opensearch index settings
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * addressing comment
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adding malformed settings unit test
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * fix formatting issues
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * misc dev changes (unpolished, for testing purposes)
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adding fast refresh check
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * formatting
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adding inspect exception
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * formatting
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adjusting setting name
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * cleanup
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * even more cleanup
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * even more cleanup
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * runback 1
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * runback 2
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * adding malformed settings docker image test
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * polishing malformed settings and mappings docker image tests
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * cleanup
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    ---------
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    Co-authored-by: Dennis Toepker <toepkerd@amazon.com>
    Signed-off-by: Aaron Alvarez <aaarone@amazon.com>

--- spark-sql-application/src/main/scala/org/apache/spark/sql/FlintJobExecutor.scala
+++ spark-sql-application/src/main/scala/org/apache/spark/sql/FlintJobExecutor.scala
@@ -108,6 +108,15 @@ trait FlintJobExecutor {
       }
     }""".stripMargin
 
+  // Fast refresh index setting for OpenSearch index. Eliminates index refresh as a source
+  // of latency for interactive queries.
+  val resultIndexSettings =
+    """{
+      "index": {
+        "refresh_interval": "1s"
+      }
+    }""".stripMargin
+
   // Define the data schema
   val schema = StructType(
     Seq(
@@ -199,7 +208,7 @@ trait FlintJobExecutor {
     if (osClient.doesIndexExist(resultIndex)) {
       writeData(resultData, resultIndex, refreshPolicy)
     } else {
-      createResultIndex(osClient, resultIndex, resultIndexMapping)
+      createResultIndex(osClient, resultIndex, resultIndexMapping, resultIndexSettings)
       writeData(resultData, resultIndex, refreshPolicy)
     }
   }
@@ -375,7 +384,7 @@ trait FlintJobExecutor {
       case e: IllegalStateException
           if e.getCause != null &&
             e.getCause.getMessage.contains("index_not_found_exception") =>
-        createResultIndex(osClient, resultIndex, resultIndexMapping)
+        createResultIndex(osClient, resultIndex, resultIndexMapping, resultIndexSettings)
       case e: InterruptedException =>
         val error = s"Interrupted by the main thread: ${e.getMessage}"
         Thread.currentThread().interrupt() // Preserve the interrupt status
@@ -391,10 +400,11 @@ trait FlintJobExecutor {
   def createResultIndex(
       osClient: OSClient,
       resultIndex: String,
-      mapping: String): Either[String, Unit] = {
+      mapping: String,
+      settings: String): Either[String, Unit] = {
     try {
       logInfo(s"create $resultIndex")
-      osClient.createIndex(resultIndex, mapping)
+      osClient.createIndex(resultIndex, mapping, settings)
       logInfo(s"create $resultIndex successfully")
       Right(())
     } catch {
