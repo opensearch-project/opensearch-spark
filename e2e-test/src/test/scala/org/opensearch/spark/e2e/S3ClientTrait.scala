d68b47ebd4c7f687f65cbef97e4a5e1dd99d2f25
Aaron Alvarez <aaarone@amazon.com>
2025-05-01 21:16:51 +0000

    Improve E2E Test Suite logic
    Signed-off-by: Aaron Alvarez <aaarone@amazon.com>

--- e2e-test/src/test/scala/org/opensearch/spark/e2e/S3ClientTrait.scala
+++ e2e-test/src/test/scala/org/opensearch/spark/e2e/S3ClientTrait.scala
@@ -8,11 +8,14 @@ package org.opensearch.spark.e2e
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import com.amazonaws.services.s3.model.HeadBucketRequest
+import org.apache.logging.log4j.{Logger, LogManager}
 
 /**
  * Provides a method for obtaining an S3 client.
  */
 trait S3ClientTrait {
+  private val log: Logger = LogManager.getLogger(getClass)
   var s3Client : AmazonS3 = null
 
   /**
@@ -48,6 +51,42 @@ trait S3ClientTrait {
       }
 
       s3Client
+    }
+  }
+
+   /**
+   * Checks if an S3 bucket exists and is accessible
+   *
+   * @param bucketName name of the bucket to check
+   * @return true if bucket exists and is accessible, false otherwise
+   */
+  def doesBucketExist(bucketName: String): Boolean = {
+    try {
+      getS3Client().headBucket(new HeadBucketRequest(bucketName))
+      true
+    } catch {
+      case e: com.amazonaws.services.s3.model.AmazonS3Exception =>
+        log.error(s"Error checking bucket '$bucketName': ${e.getMessage}")
+        false
+    }
+  }
+
+  /**
+   * Ensures a bucket exists, creates it if it doesn't
+   *
+   * @param bucketName name of the bucket to check/create
+   */
+  def ensureBucketExists(bucketName: String): Unit = {
+    if (!doesBucketExist(bucketName)) {
+      log.info(s"Bucket '$bucketName' does not exist or is not accessible")
+      try {
+        getS3Client().createBucket(bucketName)
+        log.info(s"Created bucket '$bucketName'")
+      } catch {
+        case e: Exception =>
+          log.error(s"Failed to create bucket: ${e.getMessage}")
+          throw e
+      }
     }
   }
 }
