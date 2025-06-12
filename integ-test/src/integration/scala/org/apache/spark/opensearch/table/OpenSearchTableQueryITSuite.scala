672d53c8777e04af640a66458b3874174e057a44
Simeon Widdis <sawiddis@amazon.com>
2025-06-12 17:34:36 +0000

    Remove another half_float test
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- integ-test/src/integration/scala/org/apache/spark/opensearch/table/OpenSearchTableQueryITSuite.scala
+++ integ-test/src/integration/scala/org/apache/spark/opensearch/table/OpenSearchTableQueryITSuite.scala
@@ -11,9 +11,8 @@ import org.opensearch.flint.spark.udt.{GeoPoint, IPAddress, IPFunctions}
 import org.apache.spark.sql.{DataFrame, ExplainSuiteHelper, Row, SparkSession}
 import org.apache.spark.sql.execution.datasources.v2.DataSourceV2ScanRelation
 
-/**
- * Test queries on OpenSearch Table
- */
+/** Test queries on OpenSearch Table
+  */
 class OpenSearchTableQueryITSuite
     extends OpenSearchCatalogSuite
     with FlintPPLSuite
@@ -47,7 +46,8 @@ class OpenSearchTableQueryITSuite
 
         val df = spark.sql(
           s"source = $factTbl | stats count() by accountId " +
-            s"| LOOKUP $lookupTbl accountId REPLACE eventSource")
+            s"| LOOKUP $lookupTbl accountId REPLACE eventSource"
+        )
         checkAnswer(df, Row(1, "123", "source"))
       }
     }
@@ -58,15 +58,20 @@ class OpenSearchTableQueryITSuite
     withIndexName(index1) {
       indexWithAlias(index1)
       // select original field and alias field
-      var df = spark.sql(s"""SELECT id, alias FROM ${catalogName}.default.$index1""")
+      var df =
+        spark.sql(s"""SELECT id, alias FROM ${catalogName}.default.$index1""")
       checkAnswer(df, Seq(Row(1, 1), Row(2, 2)))
 
       // filter on alias field
-      df = spark.sql(s"""SELECT id, alias FROM ${catalogName}.default.$index1 WHERE alias=1""")
+      df = spark.sql(
+        s"""SELECT id, alias FROM ${catalogName}.default.$index1 WHERE alias=1"""
+      )
       checkAnswer(df, Row(1, 1))
 
       // filter on original field
-      df = spark.sql(s"""SELECT id, alias FROM ${catalogName}.default.$index1 WHERE id=1""")
+      df = spark.sql(
+        s"""SELECT id, alias FROM ${catalogName}.default.$index1 WHERE id=1"""
+      )
       checkAnswer(df, Row(1, 1))
     }
   }
@@ -79,18 +84,26 @@ class OpenSearchTableQueryITSuite
       // Validate that an exact equality query on the multi-field 'aTextString'
       // is pushed down to OpenSearch and returns the matching document.
       var df =
-        spark.sql(s"""SELECT id FROM $table WHERE aTextString = "Treviso-Sant'Angelo Airport" """)
-      checkPushedInfo(df, "aTextString IS NOT NULL, aTextString = 'Treviso-Sant'Angelo Airport'")
+        spark.sql(
+          s"""SELECT id FROM $table WHERE aTextString = "Treviso-Sant'Angelo Airport" """
+        )
+      checkPushedInfo(
+        df,
+        "aTextString IS NOT NULL, aTextString = 'Treviso-Sant'Angelo Airport'"
+      )
       checkAnswer(df, Seq(Row(1)))
 
       // Validate that an equality query on 'aTextString' with a non-exact match returns no results.
-      df = spark.sql(s"""SELECT id FROM $table WHERE aTextString = "Airport" """)
+      df =
+        spark.sql(s"""SELECT id FROM $table WHERE aTextString = "Airport" """)
       checkPushedInfo(df, "aTextString IS NOT NULL, aTextString = 'Airport'")
       checkAnswer(df, Seq())
     }
   }
 
-  test("Query text fields index - Combined conditions on text and keyword fields") {
+  test(
+    "Query text fields index - Combined conditions on text and keyword fields"
+  ) {
     val indexName = "t0001"
     val table = s"${catalogName}.default.$indexName"
     withIndexName(indexName) {
@@ -99,15 +112,21 @@ class OpenSearchTableQueryITSuite
       // Validate that the condition on the text field 'aText', which is evaluated by Spark
       // without push down to OpenSearch, returns the expected document
       var df =
-        spark.sql(s"""SELECT id FROM $table WHERE aText = "Treviso-Sant'Angelo Airport" """)
+        spark.sql(
+          s"""SELECT id FROM $table WHERE aText = "Treviso-Sant'Angelo Airport" """
+        )
       checkPushedInfo(df, "aText IS NOT NULL")
       checkAnswer(df, Seq(Row(1)))
 
       // Validate that the condition on the text field 'aText', which is evaluated by Spark,
       // aString is push down to OpenSearch returns the expected document when both conditions are met.
       df = spark.sql(
-        s"""SELECT id FROM $table WHERE aText = "Treviso-Sant'Angelo Airport" AND aString = "OpenSearch-Air" """)
-      checkPushedInfo(df, "aText IS NOT NULL, aString IS NOT NULL, aString = 'OpenSearch-Air'")
+        s"""SELECT id FROM $table WHERE aText = "Treviso-Sant'Angelo Airport" AND aString = "OpenSearch-Air" """
+      )
+      checkPushedInfo(
+        df,
+        "aText IS NOT NULL, aString IS NOT NULL, aString = 'OpenSearch-Air'"
+      )
       checkAnswer(df, Seq(Row(1)))
 
       // Validate that a query with a full string equality condition on 'aText'
@@ -131,31 +150,21 @@ class OpenSearchTableQueryITSuite
         Seq(
           Row(1, GeoPoint(40.12, -71.34)),
           Row(2, GeoPoint(40.12, -71.34)),
-          Row(3, GeoPoint(40.078125, -71.3671875)), // different value due to precision
+          Row(
+            3,
+            GeoPoint(40.078125, -71.3671875)
+          ), // different value due to precision
           Row(4, GeoPoint(40.12, -71.34)),
-          Row(5, GeoPoint(40.12, -71.34))))
+          Row(5, GeoPoint(40.12, -71.34))
+        )
+      )
     }
   }
 
   def checkPushedInfo(df: DataFrame, expectedPlanFragment: String*): Unit = {
-    df.queryExecution.optimizedPlan.collect { case _: DataSourceV2ScanRelation =>
-      checkKeywordsExistsInExplain(df, expectedPlanFragment: _*)
-    }
-  }
-
-  test("Query index with half_float data type") {
-    val indexName = "t0001"
-    val table = s"${catalogName}.default.$indexName"
-    withIndexName(indexName) {
-      indexWithNumericFields(indexName)
-
-      var df = spark.sql(s"""SELECT id, floatField, halfFloatField FROM ${table}""")
-      checkAnswer(df, Seq(Row(1, 1.1f, 1.2f), Row(2, 2.1f, 2.2f)))
-
-      df = spark.sql(
-        s"""SELECT id, floatField, halfFloatField FROM ${table} WHERE halfFloatField < 2.0""")
-      checkPushedInfo(df, "halfFloatField IS NOT NULL, halfFloatField < 2.0")
-      checkAnswer(df, Seq(Row(1, 1.1f, 1.2f)))
+    df.queryExecution.optimizedPlan.collect {
+      case _: DataSourceV2ScanRelation =>
+        checkKeywordsExistsInExplain(df, expectedPlanFragment: _*)
     }
   }
 
@@ -164,7 +173,8 @@ class OpenSearchTableQueryITSuite
     val tableName = s"""$catalogName.default.$index1"""
     val spark = SparkSession.builder().getOrCreate()
     IPFunctions.registerFunctions(spark)
-    val clientIp: Array[String] = Array("192.168.0.10", "192.168.0.11", "::ffff:192.168.0.10")
+    val clientIp: Array[String] =
+      Array("192.168.0.10", "192.168.0.11", "::ffff:192.168.0.10")
     val serverIp: Array[String] = Array("100.10.12.123", "::ffff:100.10.12.123")
 
     withIndexName(index1) {
@@ -178,34 +188,45 @@ class OpenSearchTableQueryITSuite
         Seq(
           Row(IPAddress(clientIp(0)), IPAddress(serverIp(0))),
           Row(IPAddress(clientIp(1)), IPAddress(serverIp(0))),
-          Row(IPAddress(clientIp(2)), IPAddress(serverIp(1)))))
+          Row(IPAddress(clientIp(2)), IPAddress(serverIp(1)))
+        )
+      )
 
       df = spark.sql(
-        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.10/32')")
+        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.10/32')"
+      )
       checkAnswer(df, Seq(Row(IPAddress(clientIp(0)), IPAddress(serverIp(0)))))
 
       df = spark.sql(
-        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.0/24')")
+        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.0/24')"
+      )
       checkAnswer(
         df,
         Seq(
           Row(IPAddress(clientIp(0)), IPAddress(serverIp(0))),
-          Row(IPAddress(clientIp(1)), IPAddress(serverIp(0)))))
+          Row(IPAddress(clientIp(1)), IPAddress(serverIp(0)))
+        )
+      )
 
       df = spark.sql(
-        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.0/255.255.255.0')")
+        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '192.168.0.0/255.255.255.0')"
+      )
       checkAnswer(
         df,
         Seq(
           Row(IPAddress(clientIp(0)), IPAddress(serverIp(0))),
-          Row(IPAddress(clientIp(1)), IPAddress(serverIp(0)))))
+          Row(IPAddress(clientIp(1)), IPAddress(serverIp(0)))
+        )
+      )
 
       df = spark.sql(
-        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '::ffff:192.168.0.10/128')")
+        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '::ffff:192.168.0.10/128')"
+      )
       checkAnswer(df, Seq(Row(IPAddress(clientIp(2)), IPAddress(serverIp(1)))))
 
       df = spark.sql(
-        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '::ffff:192.168.0.0/120')")
+        s"SELECT client, server FROM $tableName WHERE cidrmatch(client, '::ffff:192.168.0.0/120')"
+      )
       checkAnswer(df, Seq(Row(IPAddress(clientIp(2)), IPAddress(serverIp(1)))))
     }
   }
