6ea6ae7a0639db3de5d1b9157b26bec52332e278
Chen Dai <daichen@amazon.com>
2026-01-27 14:19:02 -0800

    Fix nullability and wrong type mapping
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- unified-query-spark-integration/src/main/scala/org/opensearch/flint/spark/query/package.scala
+++ unified-query-spark-integration/src/main/scala/org/opensearch/flint/spark/query/package.scala
@@ -21,9 +21,25 @@ import org.apache.spark.sql.types._
 package object query {
 
   /**
-   * Spark DataType to Calcite RelDataType conversion.
+   * Spark DataType to Calcite RelDataType conversion with nullability support.
    */
   implicit class SparkTypeOps(val sparkType: DataType) extends AnyVal {
+
+    /**
+     * Helper to apply nullability to a Calcite type. For struct types, we must use
+     * enforceTypeWithNullability because createTypeWithNullability pushes nullability down to
+     * fields instead of making the struct itself nullable.
+     */
+    private def withNullability(
+        typeFactory: RelDataTypeFactory,
+        calciteType: RelDataType,
+        nullable: Boolean): RelDataType = {
+      if (calciteType.isStruct) {
+        typeFactory.enforceTypeWithNullability(calciteType, nullable)
+      } else {
+        typeFactory.createTypeWithNullability(calciteType, nullable)
+      }
+    }
 
     def toCalcite(typeFactory: RelDataTypeFactory): RelDataType = sparkType match {
       case BooleanType => typeFactory.createSqlType(SqlTypeName.BOOLEAN)
@@ -33,47 +49,83 @@ package object query {
       case ShortType => typeFactory.createSqlType(SqlTypeName.SMALLINT)
       case IntegerType => typeFactory.createSqlType(SqlTypeName.INTEGER)
       case LongType => typeFactory.createSqlType(SqlTypeName.BIGINT)
-      case FloatType => typeFactory.createSqlType(SqlTypeName.FLOAT)
+      case FloatType => typeFactory.createSqlType(SqlTypeName.REAL) // 4-byte single-precision
       case DoubleType => typeFactory.createSqlType(SqlTypeName.DOUBLE)
       case dt: DecimalType =>
         typeFactory.createSqlType(SqlTypeName.DECIMAL, dt.precision, dt.scale)
 
       // String types
       case StringType => typeFactory.createSqlType(SqlTypeName.VARCHAR)
-      case _: VarcharType => typeFactory.createSqlType(SqlTypeName.VARCHAR)
-      case _: CharType => typeFactory.createSqlType(SqlTypeName.CHAR)
+      case vt: VarcharType => typeFactory.createSqlType(SqlTypeName.VARCHAR, vt.length)
+      case ct: CharType => typeFactory.createSqlType(SqlTypeName.CHAR, ct.length)
 
       // Binary type
       case BinaryType => typeFactory.createSqlType(SqlTypeName.VARBINARY)
 
       // Date/Time types
       case DateType => typeFactory.createSqlType(SqlTypeName.DATE)
-      case TimestampType => typeFactory.createSqlType(SqlTypeName.TIMESTAMP)
+      case TimestampType =>
+        typeFactory.createSqlType(SqlTypeName.TIMESTAMP_WITH_LOCAL_TIME_ZONE) // TIMESTAMP_LTZ
       case TimestampNTZType => typeFactory.createSqlType(SqlTypeName.TIMESTAMP)
 
-      // Complex types
-      case ArrayType(elementType, _) =>
-        typeFactory.createArrayType(elementType.toCalcite(typeFactory), -1)
+      // Complex types with nullability
+      case ArrayType(elementType, containsNull) =>
+        val elementCalciteType = elementType.toCalcite(typeFactory)
+        val nullableElement =
+          typeFactory.createTypeWithNullability(elementCalciteType, containsNull)
+        typeFactory.createArrayType(nullableElement, -1)
 
-      case MapType(keyType, valueType, _) =>
-        typeFactory.createMapType(
-          keyType.toCalcite(typeFactory),
-          valueType.toCalcite(typeFactory))
+      case MapType(keyType, valueType, valueContainsNull) =>
+        val keyCalciteType = keyType.toCalcite(typeFactory)
+        val valueCalciteType = valueType.toCalcite(typeFactory)
+        val nullableValue =
+          typeFactory.createTypeWithNullability(valueCalciteType, valueContainsNull)
+        typeFactory.createMapType(keyCalciteType, nullableValue)
 
       case struct: StructType =>
         val fieldNames = struct.fields.map(_.name).toList.asJava
-        val fieldTypes = struct.fields.map(_.dataType.toCalcite(typeFactory)).toList.asJava
+        val fieldTypes = struct.fields
+          .map { field =>
+            val baseType = field.dataType.toCalcite(typeFactory)
+            withNullability(typeFactory, baseType, field.nullable)
+          }
+          .toList
+          .asJava
         typeFactory.createStructType(fieldTypes, fieldNames)
 
       // Null type
       case NullType => typeFactory.createSqlType(SqlTypeName.NULL)
 
-      // Interval types
-      case CalendarIntervalType => typeFactory.createSqlType(SqlTypeName.VARCHAR)
-      case _: DayTimeIntervalType => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY_SECOND)
-      case _: YearMonthIntervalType => typeFactory.createSqlType(SqlTypeName.INTERVAL_YEAR_MONTH)
+      // Interval types - map based on (startField, endField)
+      case dt: DayTimeIntervalType =>
+        import DayTimeIntervalType._
+        (dt.startField, dt.endField) match {
+          case (DAY, DAY) => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY)
+          case (DAY, HOUR) => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY_HOUR)
+          case (DAY, MINUTE) => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY_MINUTE)
+          case (DAY, SECOND) => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY_SECOND)
+          case (HOUR, HOUR) => typeFactory.createSqlType(SqlTypeName.INTERVAL_HOUR)
+          case (HOUR, MINUTE) => typeFactory.createSqlType(SqlTypeName.INTERVAL_HOUR_MINUTE)
+          case (HOUR, SECOND) => typeFactory.createSqlType(SqlTypeName.INTERVAL_HOUR_SECOND)
+          case (MINUTE, MINUTE) => typeFactory.createSqlType(SqlTypeName.INTERVAL_MINUTE)
+          case (MINUTE, SECOND) => typeFactory.createSqlType(SqlTypeName.INTERVAL_MINUTE_SECOND)
+          case (SECOND, SECOND) => typeFactory.createSqlType(SqlTypeName.INTERVAL_SECOND)
+          case _ => typeFactory.createSqlType(SqlTypeName.INTERVAL_DAY_SECOND) // fallback
+        }
 
-      // Default fallback
+      case ym: YearMonthIntervalType =>
+        import YearMonthIntervalType._
+        (ym.startField, ym.endField) match {
+          case (YEAR, YEAR) => typeFactory.createSqlType(SqlTypeName.INTERVAL_YEAR)
+          case (YEAR, MONTH) => typeFactory.createSqlType(SqlTypeName.INTERVAL_YEAR_MONTH)
+          case (MONTH, MONTH) => typeFactory.createSqlType(SqlTypeName.INTERVAL_MONTH)
+          case _ => typeFactory.createSqlType(SqlTypeName.INTERVAL_YEAR_MONTH) // fallback
+        }
+
+      // User-defined types - delegate to underlying SQL type
+      case udt: UserDefinedType[_] => udt.sqlType.toCalcite(typeFactory)
+
+      // Default fallback for unsupported types (CalendarIntervalType, ObjectType, etc.)
       case _ => typeFactory.createSqlType(SqlTypeName.ANY)
     }
   }
--- unified-query-spark-integration/src/test/scala/org/opensearch/flint/spark/query/SparkSchemaSuite.scala
+++ unified-query-spark-integration/src/test/scala/org/opensearch/flint/spark/query/SparkSchemaSuite.scala
@@ -13,7 +13,7 @@ import org.scalatestplus.mockito.MockitoSugar
 
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types._
 
 /**
  * Test suite for SparkSchema that verifies Spark catalog to Calcite schema mapping.
@@ -67,9 +67,12 @@ class SparkSchemaSuite extends SparkFunSuite with Matchers with MockitoSugar {
         double_col DOUBLE,
         decimal_col DECIMAL(10, 2),
         string_col STRING,
+        varchar_col VARCHAR(100),
+        char_col CHAR(10),
         binary_col BINARY,
         date_col DATE,
-        timestamp_col TIMESTAMP
+        timestamp_col TIMESTAMP,
+        timestamp_ntz_col TIMESTAMP_NTZ
       """)) { spark =>
       val sparkSchema = new SparkSchema(spark, testCatalog)
       val table = sparkSchema.subSchemas().get(testDb).tables().get(testTable)
@@ -81,13 +84,53 @@ class SparkSchemaSuite extends SparkFunSuite with Matchers with MockitoSugar {
         "SMALLINT short_col, " +
         "INTEGER int_col, " +
         "BIGINT long_col, " +
-        "FLOAT float_col, " +
+        "REAL float_col, " +
         "DOUBLE double_col, " +
         "DECIMAL(10, 2) decimal_col, " +
         "VARCHAR string_col, " +
+        "VARCHAR(100) varchar_col, " +
+        "CHAR(10) char_col, " +
         "VARBINARY binary_col, " +
         "DATE date_col, " +
-        "TIMESTAMP(0) timestamp_col)"
+        "TIMESTAMP_WITH_LOCAL_TIME_ZONE(0) timestamp_col, " +
+        "TIMESTAMP(0) timestamp_ntz_col)"
+    }
+  }
+
+  test("interval types") {
+    withMockTable(Some("""
+        interval_day INTERVAL DAY,
+        interval_day_hour INTERVAL DAY TO HOUR,
+        interval_day_minute INTERVAL DAY TO MINUTE,
+        interval_day_second INTERVAL DAY TO SECOND,
+        interval_hour INTERVAL HOUR,
+        interval_hour_minute INTERVAL HOUR TO MINUTE,
+        interval_hour_second INTERVAL HOUR TO SECOND,
+        interval_minute INTERVAL MINUTE,
+        interval_minute_second INTERVAL MINUTE TO SECOND,
+        interval_second INTERVAL SECOND,
+        interval_year INTERVAL YEAR,
+        interval_year_month INTERVAL YEAR TO MONTH,
+        interval_month INTERVAL MONTH
+      """)) { spark =>
+      val sparkSchema = new SparkSchema(spark, testCatalog)
+      val table = sparkSchema.subSchemas().get(testDb).tables().get(testTable)
+
+      table.getRowType(typeFactory).toString shouldBe
+        "RecordType(" +
+        "INTERVAL_DAY(2, 6) interval_day, " +
+        "INTERVAL_DAY_HOUR(2, 6) interval_day_hour, " +
+        "INTERVAL_DAY_MINUTE(2, 6) interval_day_minute, " +
+        "INTERVAL_DAY_SECOND(2, 6) interval_day_second, " +
+        "INTERVAL_HOUR(2, 6) interval_hour, " +
+        "INTERVAL_HOUR_MINUTE(2, 6) interval_hour_minute, " +
+        "INTERVAL_HOUR_SECOND(2, 6) interval_hour_second, " +
+        "INTERVAL_MINUTE(2, 6) interval_minute, " +
+        "INTERVAL_MINUTE_SECOND(2, 6) interval_minute_second, " +
+        "INTERVAL_SECOND(2, 6) interval_second, " +
+        "INTERVAL_YEAR interval_year, " +
+        "INTERVAL_YEAR_MONTH interval_year_month, " +
+        "INTERVAL_MONTH interval_month)"
     }
   }
 
@@ -121,6 +164,66 @@ class SparkSchemaSuite extends SparkFunSuite with Matchers with MockitoSugar {
         "RecordType(INTEGER id, VARCHAR ARRAY values) ARRAY nested_array, " +
         "RecordType(RecordType(VARCHAR name, VARCHAR ARRAY tags) info) nested_struct)"
     }
+  }
+
+  test("user-defined type") {
+
+    /** Custom type that wraps a Long as its SQL type. */
+    class CustomType extends UserDefinedType[AnyRef] {
+      override def sqlType: DataType = LongType
+      override def serialize(obj: AnyRef): Any = obj
+      override def deserialize(datum: Any): AnyRef = datum.asInstanceOf[AnyRef]
+      override def userClass: Class[AnyRef] = classOf[AnyRef]
+    }
+
+    val spark = mock[SparkSession]
+    val df = mock[DataFrame]
+    val schema = new StructType().add("point_col", new CustomType)
+    when(df.schema).thenReturn(schema)
+    when(spark.table(s"$testCatalog.$testDb.$testTable")).thenReturn(df)
+
+    val sparkSchema = new SparkSchema(spark, testCatalog)
+    val table = sparkSchema.subSchemas().get(testDb).tables().get(testTable)
+
+    // PointUDT.sqlType is LongType, which maps to BIGINT
+    table.getRowType(typeFactory).toString shouldBe "RecordType(BIGINT point_col)"
+  }
+
+  test("nullability for complex and nested complex types") {
+    withMockTable(Some("""
+        array_col ARRAY<INT> NOT NULL,
+        map_col MAP<STRING, INT> NOT NULL,
+        struct_col STRUCT<name: STRING, age: INT> NOT NULL,
+        nested_array ARRAY<STRUCT<id: INT, values: ARRAY<STRING>>> NOT NULL,
+        nested_struct STRUCT<info: STRUCT<name: STRING, tags: ARRAY<STRING>>> NOT NULL
+      """)) { spark =>
+      val sparkSchema = new SparkSchema(spark, testCatalog)
+      val table = sparkSchema.subSchemas().get(testDb).tables().get(testTable)
+
+      table.getRowType(typeFactory).getFullTypeString shouldBe
+        "RecordType(" +
+        "INTEGER ARRAY NOT NULL array_col, " +
+        "(VARCHAR NOT NULL, INTEGER) MAP NOT NULL map_col, " +
+        "RecordType(VARCHAR name, INTEGER age) NOT NULL struct_col, " +
+        "RecordType(INTEGER id, VARCHAR ARRAY values) ARRAY NOT NULL nested_array, " +
+        "RecordType(RecordType(VARCHAR name, VARCHAR ARRAY tags) info) NOT NULL nested_struct) NOT NULL"
+    }
+  }
+
+  test("unsupported type fallback") {
+    val spark = mock[SparkSession]
+    val df = mock[DataFrame]
+    val schema = new StructType()
+      .add("calendar_interval_col", CalendarIntervalType)
+      .add("object_col", ObjectType(classOf[AnyRef]))
+    when(df.schema).thenReturn(schema)
+    when(spark.table(s"$testCatalog.$testDb.$testTable")).thenReturn(df)
+
+    val sparkSchema = new SparkSchema(spark, testCatalog)
+    val table = sparkSchema.subSchemas().get(testDb).tables().get(testTable)
+
+    table.getRowType(typeFactory).toString shouldBe
+      "RecordType(ANY calendar_interval_col, ANY object_col)"
   }
 
   private def withMockTable(ddl: Option[String])(f: SparkSession => Unit): Unit = {
